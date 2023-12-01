f0363080c8f143fadd40c0eb2e73825eb465bd75
sensui <sensuijunior@hotmail.com>
2023-12-01 17:28:22 -0300

    changed maid file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, request, jsonify
+from pdf2image import convert_from_path
 import fitz
 import io
 from PIL import Image
@@ -6,24 +8,20 @@ from pyzbar.pyzbar import decode
 
 app = Flask(__name__)
 
-def extract_barcode(file_stream):
-    file_bytes = file_stream.read()
-    doc = fitz.open(stream=file_bytes, filetype="pdf")
-    barcode_values = []
-    images = []
+# Function to extract images from PDF
+def extract_images_from_pdf(pdf_path):
+    # Convert PDF to a list of images
+    return convert_from_path(pdf_path)
 
-    for page_num in range(len(doc)):
-        page = doc.load_page(page_num)
-        images.extend(page.get_images())
-        pix = page.get_pixmap()
-        img = Image.open(io.BytesIO(pix.tobytes("png")))
-
-        # Decode the barcode
-        barcodes = decode(img)
-        for barcode in barcodes:
-            barcode_values.append(barcode.data.decode())
-
-    return barcode_values
+# Function to detect and decode barcodes in an image
+def decode_barcodes_from_image(image):
+    decoded_objects = decode(image)
+    barcodes = []
+    for obj in decoded_objects:
+        print('Type:', obj.type)
+        print('Data:', obj.data.decode('utf-8'))
+        barcodes.append({'type': obj.type, 'data': obj.data.decode('utf-8')})
+    return barcodes
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -33,14 +31,21 @@ def upload_file():
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
     if file:
-        barcodes = extract_barcode(file.stream)
+        # Save the file temporarily
+        temp_path = "tempfile.pdf"
+        file.save(temp_path)
+        
+        barcodes = extract_images_from_pdf(temp_path)
         if barcodes:
-            return jsonify({'barcodes': barcodes}), 200
+          decoded_barcodes = []
+          for image in barcodes:
+            decoded_barcodes.extend(decode_barcodes_from_image(image))
+            os.remove(temp_path)
+            return jsonify({'barcodes': decoded_barcodes}), 200
         else:
+            os.remove(temp_path)
             # Process the file here (e.g., check if it's a PDF, handle the content)
             return jsonify({'message': 'No barcode found'}), 200
 
-        
-        
 if __name__ == '__main__':
     app.run(debug=True)
